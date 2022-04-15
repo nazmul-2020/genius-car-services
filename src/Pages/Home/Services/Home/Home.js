0fab0c73a9c529528359b85a526c422b70dd77dd
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-04-15 14:15:46 +0600

    responsive using bootstrap

--- src/Pages/Home/Services/Home/Home.js
+++ src/Pages/Home/Services/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Experts from '../../Experts/Experts';
 import Services from '../Services';
 
 const Hone = () => {
     return (
         <div>
             <Services></Services>
+            <Experts></Experts>
         </div>
     );
 };
